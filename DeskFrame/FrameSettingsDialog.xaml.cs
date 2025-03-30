2a8a616221f8065968eefe3aa904fa5661725fe9
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2025-03-30 19:07:52 +0200

    Added revert option

--- DeskFrame/FrameSettingsDialog.xaml.cs
+++ DeskFrame/FrameSettingsDialog.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Win32;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Media;
@@ -8,17 +9,19 @@ namespace DeskFrame
 {
     public partial class FrameSettingsDialog : FluentWindow
     {
-        private Instance _instance;
         private DeskFrameWindow _frame;
+        private Instance _instance;
+        private Instance _originalInstance;
         private bool _isValidTitleBarColor = false;
         private bool _isValidTitleTextColor = false;
         private bool _isValidTitleTextAlignment = true;
         private bool _isValidBorderColor = false;
         private bool _isValidFileFilterRegex = true;
-
+        private bool _isReverting = false;
         public FrameSettingsDialog(DeskFrameWindow frame)
         {
             InitializeComponent();
+            _originalInstance = new Instance(frame.Instance);
             _instance = frame.Instance;
             _frame = frame;
             TitleBarColorTextBox.Text = _instance.TitleBarColor;
@@ -26,6 +29,7 @@ namespace DeskFrame
             BorderColorTextBox.Text = _instance.BorderColor;
             BorderEnabledCheckBox.IsChecked = _instance.BorderEnabled;
             TitleTextBox.Text = _instance.TitleText ?? _instance.Name;
+            _originalInstance.TitleText = TitleTextBox.Text;
             FileFilterRegexTextBox.Text = _instance.FileFilterRegex;
             TitleTextAlignmentComboBox.SelectedIndex = (int)_instance.TitleTextAlignment;
             UpdateBorderColorEnabled();
@@ -51,28 +55,25 @@ namespace DeskFrame
 
         private void ValidateSettings()
         {
+            if (_isReverting) return;
             _isValidTitleBarColor = TryParseColor(TitleBarColorTextBox.Text);
             _isValidTitleTextColor = TryParseColor(TitleTextColorTextBox.Text);
-            Debug.WriteLine($"vTitleTextColor: {_isValidTitleTextColor}");
             _isValidBorderColor = BorderEnabledCheckBox.IsChecked == true ? TryParseColor(BorderColorTextBox.Text) : true;
             _isValidFileFilterRegex = TryParseRegex(FileFilterRegexTextBox.Text);
             _isValidTitleTextAlignment = TitleTextAlignmentComboBox.SelectedIndex >= 0;
 
             if (_isValidTitleBarColor && _isValidTitleTextColor && _isValidTitleTextAlignment && _isValidBorderColor && _isValidFileFilterRegex)
             {
-                _frame.titleBar.Background = new SolidColorBrush((Color)System.Windows.Media.ColorConverter.ConvertFromString(TitleBarColorTextBox.Text));
-                _frame.title.Foreground = new SolidColorBrush((Color)System.Windows.Media.ColorConverter.ConvertFromString(TitleTextColorTextBox.Text));
-                _frame.title.Text = TitleTextBox.Text ?? _frame.Instance.Name;
-
                 _instance.TitleBarColor = TitleBarColorTextBox.Text;
                 _instance.TitleTextColor = TitleTextColorTextBox.Text;
                 _instance.BorderColor = BorderColorTextBox.Text;
                 _instance.BorderEnabled = BorderEnabledCheckBox.IsChecked == true;
-                _instance.TitleText = TitleTextBox.Text ?? _instance.Name;
-                _instance.FileFilterRegex = FileFilterRegexTextBox.Text;
                 _instance.TitleTextAlignment = (System.Windows.HorizontalAlignment)TitleTextAlignmentComboBox.SelectedIndex;
-
-                ApplyButton.IsEnabled = _isValidTitleBarColor && _isValidTitleTextColor && _isValidTitleTextAlignment && _isValidBorderColor && _isValidFileFilterRegex;
+                _instance.TitleText = TitleTextBox.Text;
+                _instance.FileFilterRegex = FileFilterRegexTextBox.Text;
+                _frame.titleBar.Background = new SolidColorBrush((Color)System.Windows.Media.ColorConverter.ConvertFromString(TitleBarColorTextBox.Text));
+                _frame.title.Foreground = new SolidColorBrush((Color)System.Windows.Media.ColorConverter.ConvertFromString(TitleTextColorTextBox.Text));
+                _frame.title.Text = TitleTextBox.Text ?? _frame.Instance.Name;
             }
         }
 
@@ -121,6 +122,45 @@ namespace DeskFrame
                 this.DialogResult = true;
                 this.Close();
             }
+        }
+
+        private async void RevertButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            var dialog = new Wpf.Ui.Controls.MessageBox
+            {
+                Title = "Confirm",
+                Content = "Are you sure you want to revert it?",
+                PrimaryButtonText = "Yes",
+                CloseButtonText = "No"
+            };
+
+            var result = await dialog.ShowDialogAsync();
+
+            if (result == Wpf.Ui.Controls.MessageBoxResult.Primary)
+            {
+                _isReverting = true;
+                _instance.TitleBarColor = _originalInstance.TitleBarColor;
+                _instance.TitleTextColor = _originalInstance.TitleTextColor;
+                _instance.BorderColor = _originalInstance.BorderColor;
+                _instance.BorderEnabled = _originalInstance.BorderEnabled;
+                _instance.TitleText = _originalInstance.TitleText ?? _originalInstance.Name;
+                _instance.FileFilterRegex = _originalInstance.FileFilterRegex;
+                _instance.TitleTextAlignment = _originalInstance.TitleTextAlignment;
+
+                TitleBarColorTextBox.Text = _instance.TitleBarColor;
+                TitleTextColorTextBox.Text = _instance.TitleTextColor;
+                BorderColorTextBox.Text = _instance.BorderColor;
+                BorderEnabledCheckBox.IsChecked = _instance.BorderEnabled;
+                TitleTextBox.Text = _instance.TitleText ?? _instance.Name;
+                FileFilterRegexTextBox.Text = _instance.FileFilterRegex;
+                TitleTextAlignmentComboBox.SelectedIndex = (int)_instance.TitleTextAlignment;
+
+                UpdateBorderColorEnabled();
+                _isReverting = false;
+                ValidateSettings();
+            }
+          
         }
     }
 }
