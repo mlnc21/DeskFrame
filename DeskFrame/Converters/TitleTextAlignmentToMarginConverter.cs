415b8b6c7618f6313469681d9c17fbde2c49192f
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2025-04-03 20:29:53 +0200

    Adjust horizontal alignment on title

---
+++ DeskFrame/Converters/TitleTextAlignmentToMarginConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using HorizontalAlignment = System.Windows.HorizontalAlignment;
+
+namespace DeskFrame
+{
+    public class TitleTextAlignmentToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is HorizontalAlignment alignment)
+            {
+                switch (alignment)
+                {
+                    case HorizontalAlignment.Left:
+                        return new Thickness(0, 0, 0, 0); 
+                    case HorizontalAlignment.Right:
+                        return new Thickness(0, 0, 20, 0); 
+                    default:
+                        return new Thickness(0); 
+                }
+            }
+            return new Thickness(0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
