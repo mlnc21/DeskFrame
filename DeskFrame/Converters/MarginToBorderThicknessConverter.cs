9e822b97b2966387db2d303cc0fa14a9bece4176
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2025-04-08 19:10:31 +0200

    Border adjustments

--- DeskFrame/Converters/MarginToBorderThicknessConverter.cs
+++ DeskFrame/Converters/MarginToBorderThicknessConverter.cs
@@ -4,13 +4,13 @@ using System.Windows.Data;
 
 namespace DeskFrame
 {
-    public class BooleanToBorderThicknessConverter : IValueConverter
+    public class MarginToBorderThicknessConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool enabled)
             {
-                return enabled ? new Thickness(1.3) : new Thickness(0);
+                return enabled ? new Thickness(-1.3) : new Thickness(0);
             }
             return new Thickness(0);
         }
