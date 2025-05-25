5e639efede5f565da9755ff76332200fdb064b26
PinchToDebug <52333417+PinchToDebug@users.noreply.github.com>
2025-05-25 19:31:08 +0200

    Add update progress toast

--- DeskFrame/Util/Updater.cs
+++ DeskFrame/Util/Updater.cs
@@ -3,12 +3,16 @@ using System.IO;
 using System.Text.Json;
 using System.Reflection;
 using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.Notifications;
+using System.Net.Http;
+using System.Globalization;
 namespace DeskFrame
 {
     public class Updater
     {
         private static string _url = "";
         private static string _downloadUrl = "";
+        private static string tag_name = "";
         private static int updateCount = 0;
         public static async Task CheckUpdateAsync(string url, bool showToastIfNoUpdate)
         {
@@ -25,6 +29,7 @@ namespace DeskFrame
                     {
                         var root = doc.RootElement;
                         string latestVersion = root.GetProperty("tag_name").GetString()!;
+                        tag_name = latestVersion;
                         string description = root.GetProperty("body").GetString()!;
                         string published_at = root.GetProperty("published_at").GetString()!;
                         string name = root.GetProperty("name").GetString()!;
@@ -53,7 +58,7 @@ namespace DeskFrame
                                      .SetBackgroundActivation());
                             toastBuilder.Show();
                         }
-                        else if(showToastIfNoUpdate)
+                        else if (showToastIfNoUpdate)
                         {
                             var toastBuilder = new ToastContentBuilder()
                                .AddText("You are up to date!", AdaptiveTextStyle.Header)
@@ -78,13 +83,74 @@ namespace DeskFrame
         }
         public static async Task InstallUpdate()
         {
-            
-            using (var httpClient = new System.Net.Http.HttpClient())
+            string tag = "update";
+            string group = "downloads";
+
+            var toast = new ToastContentBuilder()
+                .AddText($"Updating to {tag_name}", AdaptiveTextStyle.Header)
+                .AddVisualChild(new AdaptiveProgressBar()
+                {
+                    Title = $"Progress",
+                    Value = new BindableProgressBarValue("progressValue"),
+                    ValueStringOverride = new BindableString("progressValueString"),
+                    Status = new BindableString("progressStatus")
+                })
+                .GetToastContent();
+
+            var notif = new ToastNotification(toast.GetXml())
             {
-                Debug.WriteLine($"{Process.GetCurrentProcess().MainModule!.FileVersionInfo.FileVersion}");
-                var latestFileBytes = await httpClient.GetByteArrayAsync(_downloadUrl);
+                Tag = tag,
+                Group = group
+            };
+            ToastNotificationManagerCompat.CreateToastNotifier().Show(notif);
+
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync(_downloadUrl, HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
+                var totalBytes = response.Content.Headers.ContentLength ?? -1L;
+                var canReportProgress = totalBytes != -1;
+
                 string tempFilePath = Path.Combine(Path.GetTempPath(), $"{Assembly.GetExecutingAssembly().GetName().Name}.exe");
-                File.WriteAllBytes(tempFilePath, latestFileBytes);
+
+                using (var inputStream = await response.Content.ReadAsStreamAsync())
+                using (var outputStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    var buffer = new byte[8192];
+                    long totalRead = 0;
+                    int read;
+                    int lastProgress = -1;
+
+                    while ((read = await inputStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await outputStream.WriteAsync(buffer, 0, read);
+                        totalRead += read;
+
+                        if (canReportProgress)
+                        {
+                            int progress = (int)(((long)totalRead * 100) / totalBytes);
+                            if (progress != lastProgress)
+                            {
+                                lastProgress = progress;
+
+                                var data = new NotificationData
+                                {
+                                    SequenceNumber = (uint)progress,
+                                    Values =
+                                        {
+                                            ["progressValue"] = (progress / 100.0).ToString("0.##", CultureInfo.InvariantCulture),
+                                            ["progressValueString"] = $"{progress}%",
+                                            ["progressStatus"] = "Downloading..."
+                                        }
+                                };
+
+                                ToastNotificationManagerCompat.CreateToastNotifier().Update(data, tag, group);
+
+                            }
+                        }
+                    }
+                }
+                ToastNotificationManagerCompat.CreateToastNotifier().Hide(notif);
                 RestartApplication(tempFilePath);
             }
         }
